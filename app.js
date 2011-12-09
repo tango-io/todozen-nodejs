787bde5fac71c4192d13abec1fbd59bea784e309
Narciso Guillen <narciso.guillen@tangosource.com>
2011-12-09 17:07:58 -0600

    Start up index

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 var express = require('express')
-  , app = express.createServer();
+  , app = express.createServer()
+  , jade = require('jade')
+  , routes = require('./routes');
+
+// Configuration
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+//
+
+
 
 app.get('/', function(req, res){
   res.send('To do zone');
