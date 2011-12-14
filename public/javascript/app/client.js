fa6eaeca6479a2582ace3ceac5cff8c939878ce7
Narciso Guillen <narciso.guillen@tangosource.com>
2011-12-14 10:36:46 -0600

    starting tags for stories

--- public/javascript/app/client.js
+++ public/javascript/app/client.js
@@ -1,7 +1,9 @@
 (function($){
   Backbone.sync = function(method, model, success, error){ 
     success();
-  }
+  };
+
+  COLUMNS = ["inbox", "working", "complete"];
 
   listView = new ListView();
 })(jQuery);
