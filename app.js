aeb2b95e55e478ff941ca50cbb3fdf5219224c34
Narciso Guillen <narciso.guillen@tangosource.com>
2011-12-15 13:02:46 -0600

    build up socket module

--- app.js
+++ app.js
@@ -2,40 +2,12 @@ var express = require('express')
   , app = express.createServer()
   , jade = require('jade') 
   , routes = require('./routes')
-  , io = require('socket.io').listen(app);
+  //, io = require('socket.io').listen(app)
+  , socket = require('./socket-app');
 
-var users = [];
 
-io.sockets.on('connection', function (socket) {
-  console.log('Someone just connected!');
-    socket.on('set nickname', function (nickname, callback) {
-        if (users.indexOf(nickname) !== -1) {
-            return callback(false);
-        }
-
-        socket.nickname = nickname;
-
-        users.push(nickname);
-        socket.broadcast.emit('user join', nickname);
-        socket.emit('users', users);
-        console.log('Wellcome: '+ nickname + ' !');
-        callback(true);
-    });
-  
-    socket.on('message', function (msg) {
-        socket.broadcast.emit('user message', socket.nickname, msg);
-    });
-  
-    socket.on('disconnect', function () {
-        var nickname = socket.nickname;
-        if (nickname) {
-            socket.broadcast.emit('user part', nickname);
-            users.splice(users.indexOf(nickname), 1);
-            console.log('Good bye: '+ nickname + ' !');
-        }
-    });
-});
-
+  io = require('socket.io').listen(app);
+  socket.start();
 
 
 // Configuration
---
+++ socket-app.js
@@ -0,0 +1,33 @@
+exports.start = function(){
+var users = [];
+
+io.sockets.on('connection', function (socket) {
+  console.log('Someone just connected!');
+    socket.on('set nickname', function (nickname, callback) {
+        if (users.indexOf(nickname) !== -1) {
+            return callback(false);
+        }
+
+        socket.nickname = nickname;
+
+        users.push(nickname);
+        socket.broadcast.emit('user join', nickname);
+        socket.emit('users', users);
+        console.log('Wellcome: '+ nickname + ' !');
+        callback(true);
+    });
+  
+    socket.on('message', function (msg) {
+        socket.broadcast.emit('user message', socket.nickname, msg);
+    });
+  
+    socket.on('disconnect', function () {
+        var nickname = socket.nickname;
+        if (nickname) {
+            socket.broadcast.emit('user part', nickname);
+            users.splice(users.indexOf(nickname), 1);
+            console.log('Good bye: '+ nickname + ' !');
+        }
+    });
+});
+}
